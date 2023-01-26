500d918602c1d9dc6b72c71bf7a84cd2043930aa
amminash <alhassanaminatu12@gmail.com>
2023-01-26 23:22:45 +0000

    changes

--- java.js
+++ java.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY > 0);
 })
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
